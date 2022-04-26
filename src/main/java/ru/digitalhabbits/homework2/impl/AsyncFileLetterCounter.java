ee83a61cae5368451e26001eab4861f82d91a59a
alaryushkin <alaryushkin@icicall.com>
2022-04-26 13:01:25 +0300

    Fix: Count characters sums between lines perform in separate threads

--- src/main/java/ru/digitalhabbits/homework2/impl/AsyncFileLetterCounter.java
+++ src/main/java/ru/digitalhabbits/homework2/impl/AsyncFileLetterCounter.java
@@ -12,13 +12,13 @@ import ru.digitalhabbits.homework2.LetterCounter;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
-import static java.util.stream.Collector.of;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toCollection;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PACKAGE, makeFinal = true)
@@ -31,21 +31,35 @@ public class AsyncFileLetterCounter implements FileLetterCounter {
 
     @Override
     public Map<Character, Long> count(@NonNull File input) {
-        List<Future<Map<Character, Long>>> numberOfLetters = reader.readLines(input)
+        Queue<Future<Map<Character, Long>>> numberOfLetters = reader.readLines(input)
                 .map(this::countInThread)
-                .collect(toList());
+                .collect(toCollection(LinkedList::new));
 
-        var collector = of(HashMap::new,
-                merger::merge,
-                merger::merge);
+        if (numberOfLetters.isEmpty()) {
+            return new HashMap<>();
+        }
 
-        return numberOfLetters.stream()
-                .map(this::get)
-                .collect(collector);
+        while (numberOfLetters.size() > 1) {
+            var firstCount = numberOfLetters.poll();
+            var secondCount = numberOfLetters.poll();
+            var mergedCount = mergeInThread(firstCount, secondCount);
+            numberOfLetters.add(mergedCount);
+        }
+
+        var result = numberOfLetters.poll();
+        return get(result);
     }
 
     private Future<Map<Character, Long>> countInThread(String line) {
         return executorService.submit(() -> letterCounter.count(line));
+    }
+
+    private Future<Map<Character, Long>> mergeInThread(Future<Map<Character, Long>> firstCount,
+                                                       Future<Map<Character, Long>> secondCount) {
+        return executorService.submit(() -> merger.merge(
+                get(firstCount),
+                get(secondCount)
+        ));
     }
 
     @SneakyThrows
